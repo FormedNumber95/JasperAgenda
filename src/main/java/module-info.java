efc2607700beed5f4afdedfec4d55094fbcb5b72
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-12-09 12:26:04 +0100

    añadida la prueba del primer reporte de la actividad
    ahora hay que cambiar la interfaz grafica y reutilizar el codigo escrito

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module es.aketzagonzalez {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires java.sql;
+    requires net.sf.jasperreports.core;
     opens es.aketzagonzalez.jasperAgenda to javafx.fxml;
     exports es.aketzagonzalez.jasperAgenda;
 }
